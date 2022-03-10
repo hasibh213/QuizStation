83ac14b6e367417284f45268f99323a19481357f
Hasib Hussain <hasibh213@gmail.com>
2022-03-10 16:42:50 +0000

    homepage routing and basic html done

---
+++ client/src/components/homepage/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <h1 className="homepage__title">WELCOME TO QUIZSTATION</h1>
+
+      {/* <Link to="/QuestionPage.js"> */}
+      <button className="homepage__button">
+        <p className="homepage__button-text">START</p>
+      </button>
+      {/* </Link> */}
+    </div>
+  );
+}
+
+export default Homepage;
