bfdb1fa3c0407d5bb4b65c2916bec6587c88f943
Hasib Hussain <hasibh213@gmail.com>
2022-03-10 17:14:49 +0000

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./App.scss";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/homepage/HomePage.js";
 import QuestionPage from "./components/questionpage/QuestionPage.js";
+import OutcomePage from "./components/outcomepage/OutcomePage.js";
+import ClosingPage from "./components/closingpage/ClosingPage.js";
 
 export default function App() {
   return (
     <div className="app">
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/question" component={QuestionPage} />
         <Route
-          path="/videos/:id"
-          render={(routerProps) => <Homepage {...routerProps} />} */}
+          path="/question/:id"
+          render={(routerProps) => <QuestionPage {...routerProps} />}
         />
+        <Route path="/outcome" component={OutcomePage} />
+        <Route path="/closing" component={ClosingPage} />
       </Switch>
     </div>
   );
