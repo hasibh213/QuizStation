ecc6bd580a88896c265cbdd0585d20a1b19b69b5
Hasib Hussain <hasibh213@gmail.com>
2022-03-11 13:06:29 +0000

    front page done

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./homepage.scss";
 
 function Homepage() {
   return (
     <div className="homepage">
+      {/* <img
+        className="homepage__background"
+        src={londonbackground}
+        alt="london"
+      /> */}
       <h1 className="homepage__title">WELCOME TO QUIZSTATION</h1>
-      <Link to="/question/1">
-        <button className="homepage__button">
-          <p className="homepage__button-text">START</p>
-        </button>
-      </Link>
+      <div className="homepage__footer">
+        <h2 className="homepage__speechbox sb2">
+          Welcome Grasshopper! To make it in london as a web dev, you're gonna
+          need to practice your skills! Lets put you to the test..
+        </h2>
+        <Link to="/question/1">
+          <button className="homepage__button">
+            <p className="homepage__button-text">START</p>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
